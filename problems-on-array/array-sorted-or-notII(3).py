0f0b5cf61bc471c1028e0950db671d8fb45c6186
Surydevx <219320689+Surydevx@users.noreply.github.com>
2026-07-15 22:09:52 +0530

    Third question-optimised

--- problems-on-array/array-sorted-or-notII(3).py
+++ problems-on-array/array-sorted-or-notII(3).py
@@ -1,9 +1,6 @@
 class Solution:
     def isSorted(self, nums):
-        x = 1
-        for w in range(len(nums)-1):
-            if nums[w] > nums[w+1]:
-                x = 0
-            else:
-                pass
-        return bool(x)
+        for w in range(len(nums) - 1):
+            if nums[w] > nums[w + 1]:
+                return False
+        return True
